25a8de87136e8d2e22228fedc15f9d68f46dca4f
Deka Julian Arrizki <kamildeka123@gmail.com>
2022-12-24 14:20:27 +0700

    [UPDATE] provision authentication, server already receive n2 from client

--- a.py
+++ a.py
@@ -6,13 +6,12 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_der_public_key
 
-HEADER = 1028
 PORT = 5050
-FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
 SERVER = '127.0.0.1'
 ADDR = (SERVER, PORT)
 ID = None
+N1 = None
 
 PRIVATE_KEY = rsa.generate_private_key(65537, 2048)
 PUBLIC_KEY = PRIVATE_KEY.public_key()
@@ -24,12 +23,14 @@ def printMenuOptions():
     print("\t Enter 'quit' to exit")
     print("\t Enter 'connect' to connect to server")
 
+
 def random10bit():
 	num = ""
 	for i in range(10):
 		rand = random.randint(0,1)
 		num += str(rand)
 	return int(num,2)
+
 
 def nonceGenerator():
 	num = ""
@@ -38,6 +39,7 @@ def nonceGenerator():
 		num += str(rand)
 	return num
 
+
 def getBPulbicKey(b_pem):
     b_pem = b_pem.decode("utf-8")
     b64data = '\n'.join(b_pem.splitlines()[1:-1])
@@ -45,6 +47,7 @@ def getBPulbicKey(b_pem):
     pub = load_der_public_key(derdata, default_backend())
 
     return pub
+
 
 def getn2(encryptedMessage):
     cert_decrypted = b''
@@ -75,11 +78,11 @@ if __name__ == '__main__':
         message = input(" -> ")
 
         if 'connect' in message :
+            # STEP 1
             b_pem = conn.recv(2048)
             PUBLIC_KEY_B = getBPulbicKey(b_pem)
-
-            n1 = nonceGenerator()
-            content = (n1 + ID).encode()
+            N1 = nonceGenerator()
+            content = (N1 + ID).encode()
 
             encryptedMessage = PUBLIC_KEY_B.encrypt(
                 content,
@@ -91,15 +94,30 @@ if __name__ == '__main__':
             )
 
             conn.send(encryptedMessage)
-
+            
+            # STEP 2
             pua = PUBLIC_KEY.public_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             )
+
             conn.send(pua)
 
-
             encryptedMessage2 = conn.recv(2048)
-            n2 = getn2(encryptedMessage2)
-            print(n2)
-            
+
+            # STEP 3
+            n2 = getn2(encryptedMessage2).encode()
+
+            encryptedMessage3 = PUBLIC_KEY_B.encrypt(
+                n2,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            )
+
+            conn.send(encryptedMessage3)
+
+        elif 'quit' in message :
+            break
--- b.py
+++ b.py
@@ -7,13 +7,14 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_der_public_key
 
-HEADER = 1028
 ADDR = ('127.0.0.1', 5050)
 FORMAT = 'utf-8'
 CONNECTIONS = dict()
 PRIVATE_KEY = rsa.generate_private_key(65537, 2048)
 PUBLIC_KEY = PRIVATE_KEY.public_key()
 PUBLIC_KEY_A = None
+N2 = None
+
 
 def nonceGenerator():
 	num = ""
@@ -45,26 +46,35 @@ def getn1(encryptedMessage):
     return n1
 
 
-# SENDING PUBLIC KEY TO A
+def getN2fromA(encryptedMessage3):
+    cert_decrypted = b''
+    cert_decrypted += PRIVATE_KEY.decrypt(
+        encryptedMessage3,
+        padding.OAEP(
+            mgf=padding.MGF1(hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    n2 = cert_decrypted[0:10].decode()
+    return n2
+
+
 def handle_client(conn, addr, client_id):
-    print("[ACK]    Assigning ID", client_id, "to client ", addr[0], ":", addr[1])
+    print("[ACK]    Assigning ID", client_id, "to ", addr[0], ":", addr[1])
     conn.send(CONNECTIONS[conn.getpeername()].encode())
 
-    print("[SEND]   Sending public key to client", client_id)
+    print("[SEND]   Sending public key to ", client_id)
     public_key_pem = PUBLIC_KEY.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
-        # encoding=serialization.Encoding.PEM,
-        # format=serialization.PrivateFormat.PKCS8,
-        # encryption_algorithm=serialization.BestAvailableEncryption(password=b'Q&s&6Zxc2b&RUeBB')
     )
     conn.send(public_key_pem)
 
     encryptedMessage1 = conn.recv(2048)
     n1 = getn1(encryptedMessage1)
-    n2 = nonceGenerator()
-    print("n2 : ", n2)
-    content = (n1 + n2).encode()
+    N2 = nonceGenerator()
+    content = (n1 + N2).encode()
 
     a_pem = conn.recv(2048)
     PUBLIC_KEY_A = getAPublicKey(a_pem)
@@ -78,10 +88,18 @@ def handle_client(conn, addr, client_id):
         )
     )
 
+    print("[SEND]   Sending encrypted message to ", client_id)
     conn.send(encryptedMessage2)
 
+    encryptedMessage3 = conn.recv(2048)
+    n2_from_client = getN2fromA(encryptedMessage3)
 
-# START SERVER AND GO TO handle_client
+    if n2_from_client == N2:
+        print("[SUCCESS]    Authentication is successful")
+    else :
+        print("[FAILED]     Authentication fails")
+
+
 def start():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
